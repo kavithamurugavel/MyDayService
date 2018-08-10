e5edc2822caf3c364da9e30d5cdae325597a6612
Kavitha Murugavel <kavi.murugavel88@gmail.com>
2018-08-10 16:48:01 -0700

    First commit: Basic weather API 'get' implementation, and inclusion of models.

---
+++ MyDayService/Controllers/WeatherController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyDayService.Models;
+using Newtonsoft.Json;
+
+namespace MyDayService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Weather")]
+    public class WeatherController : Controller
+    {
+        // GET: api/Weather
+        [HttpGet]
+        public WeatherRoot GetWeather(string zip)
+        {
+            string jsonString = "";
+            string apiEndPoint = "http://api.openweathermap.org/data/2.5/weather?zip={0}&APPID={1}";
+            string formattedAPI = string.Format(apiEndPoint, zip, "*******"); // TODO: hiding API key until encryption is coded
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(string.Format(formattedAPI));
+            webRequest.Method = "GET";
+            HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponseAsync().Result;
+
+            using (Stream stream = webResponse.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                jsonString = reader.ReadToEnd();
+            }
+
+            var weather = JsonConvert.DeserializeObject<WeatherRoot>(jsonString);
+            return weather;
+        }
+
+        // GET: api/Weather/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Weather
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Weather/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MyDayService/Models/WeatherRoot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDayService.Models
+{
+    public class WeatherRoot
+    {
+        public Coord coord { get; set; }
+        public List<Weather> weather { get; set; }
+        public Main main { get; set; }
+        public int visibility { get; set; }
+        public Wind wind { get; set; }
+        public Clouds clouds { get; set; }
+        public int dt { get; set; }
+        public Sys sys { get; set; }
+        public int id { get; set; }
+        public String name { get; set; }
+        public int cod { get; set; }
+    }
+}
